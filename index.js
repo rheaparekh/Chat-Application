b28c1439ff773f639a52e74402774718ac8038d4
Rhea Parekh <rheaparekh12@gmail.com>
2017-07-23 15:25:06 +0530

    Create basic chat application

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 var express=require('express');
 var app=express();
 var http=require('http').Server(app);
+var io=require('socket.io')(http);
 
 app.get('/',function(req,res){
   res.sendFile(__dirname+'/index.html');
-}).listen(3000);
+});
+
+io.on("connection",function(socket){
+   console.log('a user is connected');
+   socket.on('chat message',function(message){
+        io.emit('chat message',message);
+   });
+   socket.on('disconnect',function(){
+      console.log('user is now disconnected');
+   });
+});
+
+http.listen(3000,function(){
+   console.log("port 3000 connected");
+});
 
 app.use(express.static('public'))
 
